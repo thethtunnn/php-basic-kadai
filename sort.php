5a4435ed37b775070baf73f6732c577b7a66a033
THET HTUN <87650163+thethtunnn@users.noreply.github.com>
2024-05-09 00:49:19 +0900

    Add files via upload

---
+++ sort.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10];
+        function sort_2way($array, $order) {
+             //If $order is not TRUE, otherwise as FALSE
+            $order = (bool) $order;
+            if ($order) {
+                echo "昇順ソートします。true";
+                echo "<br>";
+                sort($array); 
+            } else {
+                echo "降順にソートします。false";
+                echo "<br>";
+                rsort($array); 
+            }
+            foreach ($array as $num) {
+                echo $num . "<br>";
+            }
+            return;
+        }
+        sort_2way($nums, true);
+        echo "<br>"; 
+        sort_2way($nums, false);
+        ?>
+    </p>
+</body>
+
+</html>
